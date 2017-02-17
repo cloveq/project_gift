4b7d40e65dcf6abfdbcc0a7b6c71d7e492f15ac6
cloveq <qing821@foxmail.com>
2017-02-17 13:31:16 +0800

    框架结构

---
+++ src/components/home.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="footer">
+		<ul>
+			<li><a href="">礼物说</a></li>
+			<li><a href="">热门</a></li>
+			<li><a href="">分类</a></li>
+			<li><a href="">我</a></li>
+		</ul>
+	</div>
+</template>
